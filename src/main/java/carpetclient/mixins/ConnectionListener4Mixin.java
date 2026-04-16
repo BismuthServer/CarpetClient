9a5c9e307a41b25f0b53ec946ba9b87c0f4569e6
Space Walker <spacedoesrs@gmail.com>
2026-04-16 19:58:03 +0200

    fix connection timeout mixin

--- src/main/java/carpetclient/mixins/ConnectionListener4Mixin.java
+++ src/main/java/carpetclient/mixins/ConnectionListener4Mixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 /*
 Mixing override to remove client side timeouts.
  */
-@Mixin(targets = "net.minecraft.server.network.ConnectionListener$4")
+@Mixin(targets = "net.minecraft.server.network.ConnectionListener$40061135")
 public class ConnectionListener4Mixin {
 	@ModifyConstant(method = "initChannel", constant = @Constant(intValue = 30), remap = false)
 	private int noTimeout(int value) {
